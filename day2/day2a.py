a0e298a569eae5aa4d70ce66d0f988b1414398db
Frederic Seraphine <frederic@khiz.me>
2021-12-01 17:13:58 +0100

    préparation jour 2

--- day2/day2a.py
+++ day2/day2a.py
@@ -4,16 +4,17 @@ import time
 from more_itertools import windowed
 import tracemalloc
 
-starttime = time.perf_counter()
+starttime = time.time()
 tracemalloc.start()
 
 with open('data_day1.txt') as fichier:
     i = sum(map(lambda x: int(x[0]<x[1]),windowed(map(sum, windowed(map(int,fichier),3)),2)))
 
-endtime = time.perf_counter()
+
+endtime = time.time()
 
 print("===========================")
-print("AOC 2021 - day 1 - puzzle 2")
+print("AOC 2021 - day 2 - puzzle 1")
 print(f"*** Answer : {i}")
 print(f"Temps d'éxécution: {endtime-starttime:0.6f}")
 print("Current: %d, Peak %d" % tracemalloc.get_traced_memory())
