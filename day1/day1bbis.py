678764b9a22ac861fecc0ea9ef3e292e246a1146
Frederic Seraphine <frederic@khiz.me>
2021-12-01 13:38:11 +0100

    version plus fonctionnelle mais illisible

--- day1/day1bbis.py
+++ day1/day1bbis.py
@@ -5,15 +5,11 @@ from more_itertools import windowed
 
 starttime = time.time()
 
-depths =[]
+i = sum(map(lambda x: int(x[0]<x[1]),windowed(map(sum, windowed(map(int,open('data_day1.txt')),3)),2)))
 
-i=0
-for a,b in windowed(map(sum, windowed(open('data_day1.txt'),3)),2):
-    if a < b :
-        i = i+1
-print(i)
-
+endtime = time.time()
 
 print("AOC 2021 - day 1 - puzzle 2")
+print("AOC 2021 - day 1 - puzzle 2")
 print("inc:",i)
-print("Temps d'éxécution:",time.time()-starttime)
+print("Temps d'éxécution:",endtime-starttime)
