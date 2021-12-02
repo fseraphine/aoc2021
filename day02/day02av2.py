8a6d2185f681a4faec5d9fbb4676a3bb90f78dbf
Frederic Seraphine <frederic@khiz.me>
2021-12-02 10:18:10 +0100

    version légèrement améliorée en consommation mémoire

---
+++ day02/day02av2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import time
+from more_itertools import windowed
+import tracemalloc
+
+answer = 0
+pos = { 'd': 0, 'h': 0, 'a': 0}
+
+starttime = time.time()
+tracemalloc.start()
+
+moves = []
+for line in open('data_day02.txt'):
+    move = line.strip().split(" ")
+    m,q = (move[0],int(move[1]))
+    if(m=='forward'):
+        pos['h'] = pos['h'] + q
+        pos['d'] = pos['d'] + q * pos['a']
+    elif(m=='down'):
+        pos['a'] = pos['a'] + q
+    elif(m=='up'):
+        pos['a'] = pos['a'] - q
+    #print(f"{m}\t=> h {pos['h']} - d {pos['d']} - a {pos['a']}")
+
+answer = pos['h'] * pos['d']
+
+endtime = time.time()
+
+print("===========================")
+print("AOC 2021 - day 2 - puzzle 1")
+print(f"*** Answer : {answer}")
+print(f"Temps d'éxécution: {endtime-starttime:0.6f}")
+print("Current: %d, Peak %d" % tracemalloc.get_traced_memory())
+print("===========================")
